7ee524b0d7fe2ed042a77b2fe2b597529f6c4608
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-07-15 16:43:12 +0530

    clean up for new redux project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './assets/image/logo.png';
 import './App.css';
-import TaskList from './components/TaskList/TaskList';
 
 function App() {
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <TaskList />
     </div>
   );
 }
